53ad492ce17ae4aff2b5c3ce045885fc9d5408e6
hirsch <hirsch@7f4080cc-0fd6-0310-80de-91c3c2ff37c6>
2004-03-29 19:49:45 +0000

    - Added class CRunParser to deserialize stored XML rundatabase

---
+++ philippides/src/RunParser.cpp
@@ -0,0 +1,161 @@
+//******************************************************************************
+/**
+ * @file RunParser.cpp
+ * short description.
+ * brief description.
+ *
+ * @author Falco Hirschenberger <hirsch@bigfoot.de>
+ * @date Mar/28/2004
+ *
+ * (c) Falco Hirschenberger <hirsch@bigfoot.de>
+ **/
+//******************************************************************************
+
+
+//------------------------------------------------------------------------------
+// STL headers
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// xyzlib headers
+//------------------------------------------------------------------------------
+// headers for each lib should have own section
+
+
+//------------------------------------------------------------------------------
+// local headers
+//------------------------------------------------------------------------------
+#include "RunParser.h"
+#include "Run.h"
+#include "Exceptions.h"
+
+//------------------------------------------------------------------------------
+// macros
+//------------------------------------------------------------------------------
+
+
+//******************************************************************************
+// implementation
+//******************************************************************************
+namespace Phil
+{
+
+//------------------------------------------------------------------------------
+// structors
+//------------------------------------------------------------------------------
+CRunParser::CRunParser(QPtrList<CRun>* pRunList):
+    m_pRun(0),
+    m_pRunList(pRunList)
+{
+    m_pRunList->clear();
+}
+
+CRunParser::~CRunParser()
+{
+    /* EMPTY because the run object is not ours */ 
+}
+
+//------------------------------------------------------------------------------
+// accessors
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// operator
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// methods
+//------------------------------------------------------------------------------
+bool CRunParser::startElement(const QString& /* EMPTY */,
+			      const QString& /* EMPTY */,
+			      const QString& sElementName,
+			      const QXmlAttributes& attrib)
+{
+    if(sElementName == "rundb")
+	m_sVersion = attrib.value("version");
+
+    if(sElementName == "run"){
+	// if m_pRun is not null, a error occured
+	if(m_pRun){
+	    delete m_pRun;
+	    throw Except::InvalidDataException("CRunParser::startElement()",
+					       "m_pRun");
+	}
+	m_pRun = new CRun();
+    }
+
+    if(sElementName == "weather"){
+	QString sValue = attrib.value("value");
+	if(sValue == "clear")
+	    m_pRun->m_EnWeather = CRun::CLEAR;
+	else if(sValue == "cloudy")
+	    m_pRun->m_EnWeather = CRun::CLOUDY;
+	else if(sValue == "stormy")
+	    m_pRun->m_EnWeather = CRun::STORMY;
+	else if(sValue == "snowy")
+	    m_pRun->m_EnWeather = CRun::SNOWY;
+	else
+	    m_pRun->m_EnWeather = CRun::NOWEATHER;
+    }
+
+    if(sElementName == "impression"){
+	QString sValue = attrib.value("value");
+	if(sValue == "good")
+	    m_pRun->m_EnImpression = CRun::GOOD;
+	else if(sValue == "fair")
+	    m_pRun->m_EnImpression = CRun::FAIR;
+	else if(sValue == "bad")
+	    m_pRun->m_EnImpression = CRun::BAD;
+	else if(sValue == "aborted")
+	    m_pRun->m_EnImpression = CRun::ABORTED;
+	else
+	    m_pRun->m_EnImpression = CRun::NOIMPRESSION;
+    }
+return true;
+}
+ 
+bool CRunParser::characters(const QString& sText)
+{
+    m_sParsedText += sText;
+
+return true;
+}
+
+bool CRunParser::endElement(const QString& /* EMPTY */,
+			    const QString& /* EMPTY */,
+			    const QString& sElementName)
+{
+    //TODO: Do some error handling here, look if minimal requirements are present. 
+    
+    // we habe to strip the whitespaces in front and back of the string to allow
+    // functions like fromString() to work correctly, watch out for problems on 
+    // other tags with tis setting, perhaps use it exclusively on the affected
+    // tags.
+
+    m_sParsedText = m_sParsedText.stripWhiteSpace();
+    
+    if(sElementName == "date")
+	m_pRun->m_Date = QDate::fromString(m_sParsedText, Qt::ISODate);
+    else if(sElementName == "time")
+	m_pRun->m_Time = QTime::fromString(m_sParsedText, Qt::ISODate);
+    else if(sElementName == "length")
+	m_pRun->m_nLength = m_sParsedText.toUShort();
+    else if(sElementName == "pulse")
+	m_pRun->m_nPulse = m_sParsedText.toUShort();
+    else if(sElementName == "comment")
+	m_pRun->m_sComment = m_sParsedText;
+    else if(sElementName == "run"){
+        m_pRunList->append(m_pRun);
+	m_pRun = 0;
+    }
+    m_sParsedText = "";
+
+return true;
+}
+
+
+};//namespace
+
---
+++ philippides/src/RunParser.h
@@ -0,0 +1,158 @@
+//******************************************************************************
+/** @file RunParser.h
+ *  This file contains the definition of the class CRunParser
+ *
+ * @author Falco Hirschenberger <hirsch@bigfoot.de>
+ * @date Mar/28/2004
+ *
+ * (c) Falco Hirschenberger <hirsch@bigfoot.de>
+ **/
+//******************************************************************************
+
+#ifndef __PHIL_RUN_PARSER_H__
+#define __PHIL_RUN_PARSER_H__
+
+//------------------------------------------------------------------------------
+// STL headers
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// xyzlib headers
+//------------------------------------------------------------------------------
+// qt includes
+#include <qxml.h>
+
+
+//------------------------------------------------------------------------------
+// local headers
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// forwarding
+//------------------------------------------------------------------------------
+// local forwarding
+namespace Phil
+{
+    class CRun;
+}
+    
+
+//------------------------------------------------------------------------------
+// macros
+//------------------------------------------------------------------------------
+
+//******************************************************************************
+// declarations
+//******************************************************************************
+
+namespace Phil
+{
+
+/** @class CRunParser
+ * This class can parse serialized CRun objects.
+ * The class parses a XML file which must be compilant to the DTD defined
+ * in DTD::szRunDtd. It does \b NOT create a QPtrList<CRun> object, it fills
+ * the list given in the constructor. But the CRun objects pushed into the list
+ * are created by this class.
+ * 
+ * \note This class is not responsible for the QPtrList<CRun> object it fills,
+ *	 the calling class must care about it's memory.
+ *
+ * \note This class discards the possession of the created CRun objects
+ *
+ * @author Falco Hirschenberger <hirsch@bigfoot.de>
+ * @date Mar/28/2004
+ *
+ * (c) Falco Hirschenberger <hirsch@bigfoot.de>
+ **/
+class CRunParser: public QXmlDefaultHandler
+{
+    public:
+    //--------------------------------------------------------------------------
+    // types
+    //--------------------------------------------------------------------------
+	typedef CRunParser TSelf;
+   
+    //--------------------------------------------------------------------------
+    // structors
+    //--------------------------------------------------------------------------
+	/** 
+	* Constructor.
+	* The given parameter list is filled with the parsed values
+	*
+	* @param pRunList [IN/OUT] - the list to be filled with CRun objects
+	**/
+	CRunParser(QPtrList<CRun>* pRunList);
+
+	/** default destructor */
+	virtual ~CRunParser();
+
+    //--------------------------------------------------------------------------
+    // accessors
+    //--------------------------------------------------------------------------
+
+
+    //------------------------------------------------------------------------------
+    // operator
+    //------------------------------------------------------------------------------
+
+
+    //------------------------------------------------------------------------------
+    // methods
+    //------------------------------------------------------------------------------
+	/**
+	 * This method is called when a new tag starts while parsing.
+	 *
+	 * @exception Except::InvalidDataException
+	 * @param sElementName [IN] - the name of the starting tag
+	 * @param attributies [IN] - the tag's attributes
+	 * @return successful?
+	 */
+	bool startElement(const QString& /* EMPTY */, 
+			  const QString& /* EMPTY */,
+			  const QString& sElementName,
+			  const QXmlAttributes& attributes);
+
+	/**
+	 * This method is called when a end tag is parsed.
+	 *
+	 * @param sElementName [IN] - the name of the starting tag
+	 * @return successful?
+	 */
+	bool endElement(const QString& /* EMPTY */, 
+			const QString& /* EMPTY */, 
+			const QString& sElementName);
+
+	/**
+	 * This method reads strings between tags.
+	 * The strings are evaluated in endElement().
+	 *
+	 * @param sText [IN] - the read string
+	 * @return successful?
+	 */
+	bool characters(const QString& sText);
+
+
+    private:
+    //------------------------------------------------------------------------------
+    // members
+    //------------------------------------------------------------------------------
+	CRun* m_pRun;		    ///< The new CRun onject (possession discarded)
+	QPtrList<CRun>* m_pRunList; ///< The runlist to be filled with objects (not owned)
+	QString m_sVersion;	    ///< The DTD version (for use in later versions)
+	QString m_sParsedText;	    ///< The parsed text between start and end tag
+    
+    private:
+    //------------------------------------------------------------------------------
+    // implementation protection
+    //------------------------------------------------------------------------------
+	TSelf& operator=( const TSelf& other );
+	CRunParser( const TSelf& other );
+};
+
+}; //namespace
+
+#endif//__PHIL_RUN_PARSER_H__
+
